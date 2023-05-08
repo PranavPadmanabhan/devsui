b2dcc71e87e286e587299774576c3f8957f22130
PranavPadmanabhan <PranavPadmanabhan77@gmail.com>
2023-05-08 20:13:32 +0530

    fixed deploy issue

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,4 @@ export const errorColor = 'rgba(255, 0, 0, 0.6)'
 export const successColor = '#008000'
 export const normalInputColor = 'rgba(255,255,255,0.5)';
 export const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-export const usernameRegex = /^[A-Za-z][A-Za-z0-9_]{7,29}$/
+export const usernameRegex = /^[a-zA-Z0-9_]{1,15}$/
